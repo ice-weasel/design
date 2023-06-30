3c8e74e1557589b4483b6987e32a52a9b0263643
Abhishek Raman <beastonlineg@gmail.com>
2023-06-30 20:26:40 +0530

    version 1.0 (ere kore final)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -88,29 +88,29 @@ const CarbonFootprintCalculator: React.FC = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2023{" "}
             <a href="" className="hover:underline">
-              Flowbite™
+              Abhishek Raman
             </a>
             . All Rights Reserved.
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6">
+              <a href="" className="mr-4 hover:underline md:mr-6">
                 About
               </a>
             </li>
             <li>
-              <a href="#" className="mr-4 hover:underline md:mr-6">
-                Privacy Policy
+              <a href="https://github.com/ice-weasel" className="mr-4 hover:underline md:mr-6">
+                Github
               </a>
             </li>
             <li>
               <a href="#" className="mr-4 hover:underline md:mr-6">
-                Licensing
+                Linkedin
               </a>
             </li>
             <li>
               <a href="#" className="hover:underline">
-                Contact
+                
               </a>
             </li>
           </ul>
